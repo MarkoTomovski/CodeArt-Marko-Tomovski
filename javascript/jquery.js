40a3dd8300f440115fe9090192a83464e6a8f92b
Dell <Dell@DESKTOP-NGV8MRK>
2023-02-28 19:59:24 +0100

    First commit

---
+++ javascript/jquery.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+//     // Da se skrijat site odgovori osven prviot.
+//     $(".FAQ .Frame478answer").not(":first").hide();
+//
+//     // Koga kje klikneme na nekoe prashanje, da se otvori negoviot odgovor a drugite odgovori da se skrijat.
+//     $('.FAQ h1').click(function() {
+//         //Zatvori gi site odgovori
+//         $('.FAQ .Frame478answer').hide();
+//         //Otvori go odgovorot samo na toa prashanje sto se kliknato.
+//         $(this).parent().find('.Frame478answer').show();
+//     });
+// });
+// $(document).ready(function() {
+//     //koga kje go klikneme kopceto so id btn-02 da se skrolne do najbliskata sekcija za kupuvanje sto e POD kliknatoto kopche
+//     $("#btn-02").click(function() {
+//         $('html, body').animate({
+//             scrollTop: $("#CTA2").offset().top
+//         }, 1000);
+//     });
+// });
